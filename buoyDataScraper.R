f698493f4d14e9d98aa3709fade5a3bc35483ea1
karang <gabriel.g.gras@gmail.com>
2020-04-07 14:27:55 +0800

    split scraper into two files.

--- buoyDataScraper.R
+++ buoyDataScraper.R
@@ -1,13 +1,3 @@
-# To run this script:
-# update R to version 3.6
-# install docker libxml2-dev libopenssl-dev curl licurl-openssl-dev and maybe some other dependencies for RSelenium
-# sudo apt install docker.io
-# then run sudo docker pull selenium/standalone-chrome to pull the selenium docker image
-# finally run sudo docker run -d -p 4445:4444 selenium/standalone-chrome to start the container
-# run docker as a service 
-
-# docker container must be restarted before running this script!!!
-
 library(XML)
 library(htmltab)
 library(zoo)
@@ -159,79 +149,6 @@ GetXiaoLiuQiu <- function(){
   # save(小琉球浮標,file="小琉球浮標.Rda")
 }
 
-GetTideData <- function(tideUrl){
-  tideData <- htmltab(tideUrl,rm_nodata_cols = FALSE, rm_nodata_rows = FALSE)
-  tideData <- transpose(tideData)
-  colnames(tideData) <- as.character(unlist(tideData[1,]))
-  tideData <- tideData[-1,]
-  tideData[,3] <- gsub("morn-ing","morning",tideData[,3])
-  tideData[,3] <- gsub("after-noon","afternoon",tideData[,3])
-  tideData[is.na(tideData)] <- "-"
-  tideData$Time <- paste(tideData[,2],tideData[,3])
-  tideData <- tideData[,-c(1:4)]
-  if(ncol(tideData) < 16){
-    tideData <- tideData[,-c(3:10)]
-    tideData <- tideData[,c(5,1,2,3,4)]
-    
-    return(tideData)
-    
-  }else{
-    tideData <- tideData[,-c(6:13)]
-    tideData <- tideData[,c(8,1,2,6,7,3,4,5)]
-    tideData[,7] <- as.numeric(tideData[,7])
-    tideData[,8] <- as.numeric(tideData[,8])
-    tideData[,6] <- as.factor(tideData[,6])
-    tideData$Number <- as.numeric(c(1:nrow(tideData)))
-    
-    return(tideData)
-  }
-}
-
-PredTaitung <- function(){
-  TaitungTide = 'https://www.tide-forecast.com/locations/Taitung-City/forecasts/latest/six_day'
-  TaitungTideData <- GetTideData(TaitungTide)
-  # 台東潮 <- TaitungTideData
-  dfjson <- toJSON(TaitungTideData)
-  write(dfjson,file="./tideData/Taitung.JSON")
-  # save(台東潮,file="台東潮.Rda")
-}
-
-PredHualien <- function(){
-  HualienTide = 'https://www.tide-forecast.com/locations/Hualien-City/forecasts/latest/six_day'
-  HualienTideData <- GetTideData(HualienTide)
-  # 花蓮潮<- HualienTideData
-  dfjson <- toJSON(HualienTideData)
-  write(dfjson,file="./tideData/Hualien.JSON")
-  # save(花蓮潮,file="花蓮潮.Rda")
-}
-
-PredYilan <- function(){
-  ChiLungTide = 'https://www.tide-forecast.com/locations/Chi-Lung-Taiwan/forecasts/latest/six_day'
-  YilanTideData <- GetTideData(ChiLungTide)
-  # 宜蘭潮 <- YilanTideData
-  dfjson <- toJSON(YilanTideData)
-  write(dfjson,file="./tideData/Yilan.JSON")
-  # save(宜蘭潮,file="宜蘭潮.Rda")
-}
-
-PredSuAo <- function(){
-  ChiLungTide = 'https://www.tide-forecast.com/locations/Chi-Lung-Taiwan/forecasts/latest/six_day'
-  SuAoTideData <- GetTideData(ChiLungTide)
-  # 蘇澳潮 <- SuAoTideData
-  dfjson <- toJSON(SuAoTideData)
-  write(dfjson,file="./tideData/SuAo.JSON")
-  # save(蘇澳潮,file="蘇澳潮.Rda")
-}
-
-PredXiaoLiuQiu <- function(){
-  HengChunTide = 'https://www.tide-forecast.com/locations/Heng-ch-un/forecasts/latest/six_day'
-  XiaoLiuQiuTideData <- GetTideData(HengChunTide)
-  # 小琉球潮 <- XiaoLiuQiuTideData
-  dfjson <- toJSON(XiaoLiuQiuTideData)
-  write(dfjson,file="./tideData/XiaoLiuQiu.JSON")
-  # save(小琉球潮,file="小琉球潮.Rda")
-}
-
 print(as.character(now("UTC")))
 
 GetXiaoLiuQiu()
@@ -239,8 +156,3 @@ GetSuAo()
 GetYilan()
 GetHualien()
 GetTaitung()
-PredHualien()
-PredSuAo()
-PredTaitung()
-PredXiaoLiuQiu()
-PredYilan()
